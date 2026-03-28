5f7e3ddd911849a4eee88303ac82f0fbfc03865e
humbleor <1571368101@qq.com>
2026-03-28 16:10:55 +0800

    fix

--- compile.sh
+++ compile.sh
@@ -7,9 +7,3 @@ catkin_make --source Modules/fast_lio2 --build build/fast_lio2
 catkin_make --source Modules/ego_planner_swarm --build build/ego_planner_swarm
 catkin_make --source Modules/swarm_control --build build/swarm_control
 catkin_make --source Modules/realsense_ros --build build/realsense_ros
-
-# # Compile Gazebo Simulator Plugins
-# catkin_make --source Simulator/gazebo_simulator --build build/mspace_gazebo
-# catkin_make --source Simulator/realsense_gazebo_plugin --build build/realsense_gazebo_plugin
-# catkin_make --source Simulator/velodyne_gazebo_plugins --build build/velodyne_gazebo_plugins
-# catkin_make --source Simulator/livox_laser_gazebo_plugins --build build/livox_laser_gazebo_plugins
